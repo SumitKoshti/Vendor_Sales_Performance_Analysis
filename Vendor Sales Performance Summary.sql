5a7e1a39a861de4be5247526192fd8dd211a8655
Sumit Koshti <104595702+SumitKoshti@users.noreply.github.com>
2025-09-29 18:31:26 +0530

    Add files via upload

---
+++ Vendor Sales Performance Summary.sql
@@ -0,0 +1,14 @@
+USE inventorydb;
+
+SHOW TABLES;
+
+SELECT * FROM clean_vendor_sales_summary;
+
+
+SELECT COUNT(*) FROM clean_vendor_sales_summary;
+
+
+SELECT DISTINCT COUNT(VendorName) FROM clean_vendor_sales_summary;
+
+
+
